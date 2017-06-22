be20e3b95ab201294758c802de904b6295285278
naren <makernaren@gmail.com>
2017-06-22 09:11:58 +0530

    Add sqsProducer
    Pull messages from given channel and push to sqs queue

--- sqs/sqsproducer.go
+++ sqs/sqsproducer.go
@@ -1,3 +1,34 @@
 package sqsapp
 
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
 
+var producerSvc *sqs.SQS
+
+func produce(rec <-chan string, done chan bool) {
+	sess := NewSession()
+	producerSvc = sqs.New(sess)
+	qURL := "https://sqs.ap-southeast-1.amazonaws.com/971037846030/test"
+	fmt.Println("before send message")
+	for msg := range rec {
+		result, err := producerSvc.SendMessage(&sqs.SendMessageInput{
+			DelaySeconds: aws.Int64(10),
+			MessageBody:  aws.String(msg),
+			QueueUrl:     &qURL,
+		})
+		if err != nil {
+			fmt.Println("Error", err)
+			continue
+		}
+		fmt.Println("Success", *result.MessageId)
+	}
+	done <- true
+}
+
+func Push(pipe <-chan string, done chan bool) {
+	fmt.Println("starting go Produce routine")
+	produce(pipe, done)
+}
