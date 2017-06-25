058398810a275dd1dd2375d23ae21ba090b707cd
naren <makernaren@gmail.com>
2017-06-25 17:39:17 +0530

    gofmt

--- aws/app.go
+++ aws/app.go
@@ -1,12 +1,12 @@
 package aws
 
 import (
-"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/session"
 )
 
 var sess *session.Session
 
-func NewSession() (*session.Session) {
+func NewSession() *session.Session {
 	sess = session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
