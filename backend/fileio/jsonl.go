e4f8305d3b6572f6e2a05e9da5579567001821a5
naren <makernaren@gmail.com>
2017-07-09 12:57:53 +0530

    Add function to read from file
    Get function reads from jsonl files and pushes to given channel

--- backend/fileio/jsonl.go
+++ backend/fileio/jsonl.go
@@ -1,6 +1,7 @@
 package fileio
 
 import (
+	"bufio"
 	. "github.com/DudeWhoCode/kulay/logger"
 	"os"
 )
@@ -12,6 +13,24 @@ func Put(fpath string, rec <-chan string) {
 	}
 	for msg := range rec {
 		f.Write([]byte(msg + "\n"))
+	}
+	defer f.Close()
+}
+
+func Get(fpath string, snd chan<- string) {
+	f, err := os.Open(fpath)
+	Log.Info("file openend")
+	if err != nil {
+		Log.Error("Unable to open file for reading jsonl")
+	}
+	scanner := bufio.NewScanner(f)
+	Log.Info("new scanner initiaited")
+	for scanner.Scan() {
+		snd <- string(scanner.Bytes())
+		Log.Info("sending file content to channel")
+	}
+	if err := scanner.Err(); err != nil {
+		Log.Fatal("Error while scanning the file\n", err)
 	}
 	defer f.Close()
 }
