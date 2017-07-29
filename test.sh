398f9a5cf91e065707471b105c61dd11815552d3
naren <makernaren@gmail.com>
2017-07-29 14:50:59 +0530

    Create empty coverage file to avoid travis CI error

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo "" > coverage.tmp
 echo 'mode: atomic' > coverage.txt && go list ./... | grep -v vendor | xargs -n1 -I{} sh -c 'go test -covermode=atomic -coverprofile=coverage.tmp {} && tail -n +2 coverage.tmp >> coverage.txt' && rm coverage.tmp
